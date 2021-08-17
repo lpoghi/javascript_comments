4a8b9a7e6b51eb2dc956c4e9563d4819a71fc4b9
lpoghi <85479967+lpoghi@users.noreply.github.com>
2021-08-17 14:57:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hallo Winc Academy studenten")
+
+// dit is een grote som 
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* Ik ben een blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+// de code is effectief 'uitgezet'door de comment. Code die niet 'open staat zie je niet. 
